89be87c5f88f87851958adb3b00c21edbcbd6a59
harveyshao <harveyshao@tencent.com>
2022-03-26 08:00:09 +0800

    add  rest.go

---
+++ rest.go
@@ -0,0 +1,14 @@
+package ice
+
+import (
+	ice "shylinux.com/x/icebergs"
+	"shylinux.com/x/icebergs/base/web"
+	kit "shylinux.com/x/toolkits"
+)
+
+type Rest struct {
+}
+
+func (r Rest) Get(m *Message, url string, arg ...string) interface{} {
+	return kit.UnMarshal(m.Cmdx(web.SPIDE, ice.DEV, web.SPIDE_RAW, web.SPIDE_GET, url, arg))
+}
