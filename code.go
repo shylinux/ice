1d132b42f306dec0caa1b8ca93ca8713604de0ae
harveyshao <harveyshao@tencent.com>
2022-06-03 01:12:01 +0800

    add coder

--- code.go
+++ code.go
@@ -3,11 +3,15 @@ package ice
 import (
 	"path"
 	"reflect"
+	"runtime"
 	"strings"
 
 	ice "shylinux.com/x/icebergs"
+	"shylinux.com/x/icebergs/base/aaa"
 	"shylinux.com/x/icebergs/base/cli"
+	"shylinux.com/x/icebergs/base/mdb"
 	"shylinux.com/x/icebergs/base/nfs"
+	"shylinux.com/x/icebergs/base/tcp"
 	"shylinux.com/x/icebergs/base/web"
 	"shylinux.com/x/icebergs/core/code"
 	kit "shylinux.com/x/toolkits"
@@ -22,10 +26,14 @@ type Code struct {
 	order    string `name:"order" help:"定制"`
 	start    string `name:"start" help:"启动"`
 	stop     string `name:"stop" help:"停止"`
+	open     string `name:"open" help:"打开"`
 	deploy   string `name:"deploy" help:"部署"`
 	list     string `name:"list port path auto start order build download" help:"源码"`
 }
 
+func (c Code) Link(m *Message, arg ...string) string {
+	return kit.Select(m.Config(nfs.SOURCE), kit.Select(m.Config(runtime.GOOS), arg, 0))
+}
 func (c Code) Path(m *Message, url string) string {
 	return path.Join(m.Conf(code.INSTALL, kit.Keym(nfs.PATH)), kit.TrimExt(url))
 }
@@ -37,8 +45,19 @@ func (c Code) Prepare(m *Message, cb interface{}) {
 	m.Optionv(code.PREPARE, cb)
 }
 
-func (c Code) Download(m *Message, src string, arg ...string) {
-	m.Cmdy(code.INSTALL, web.DOWNLOAD, src, arg)
+func (c Code) Inputs(m *Message, arg ...string) {
+	switch arg[0] {
+	case tcp.PORT:
+		m.Cmdy(tcp.PORT)
+	case tcp.HOST:
+		m.Cmdy(tcp.HOST).Cut(aaa.IP).RenameAppend(aaa.IP, tcp.HOST)
+	}
+}
+func (c Code) Install(m *Message, arg ...string) {
+	m.Cmdy(code.INSTALL, web.DOWNLOAD, c.Link(m), kit.Slice(arg, 1))
+}
+func (c Code) Download(m *Message, arg ...string) {
+	m.Cmdy(code.INSTALL, web.DOWNLOAD, kit.Select(m.Config(nfs.SOURCE), arg, 0), kit.Slice(arg, 1))
 }
 func (c Code) Source(m *Message, src string, arg ...string) {
 	m.Cmdy(code.INSTALL, nfs.SOURCE, src, arg)
@@ -50,17 +69,36 @@ func (c Code) Order(m *Message, src, dir string, arg ...string) {
 	m.Cmd(nfs.PUSH, ice.ETC_PATH, kit.Path(m.Conf(code.INSTALL, kit.Keym(nfs.PATH)), kit.TrimExt(src), dir+ice.NL))
 	m.Cmdy(nfs.CAT, ice.ETC_PATH)
 }
-func (c Code) Start(m *Message, src string, arg ...string) {
-	m.Cmdy(code.INSTALL, cli.START, src, arg)
+func (c Code) Start(m *Message, src string, arg ...interface{}) {
+	args := []string{}
+	for _, v := range arg {
+		switch v := v.(type) {
+		case func(string) []string:
+			m.Option(code.PREPARE, v)
+		case string:
+			args = append(args, v)
+		default:
+			args = append(args, kit.Format(v))
+		}
+	}
+	m.Cmdy(code.INSTALL, cli.START, c.Link(m, src), args)
 }
-func (c Code) Stop(m *Message, src string, arg ...string) {
-	m.Cmdy(code.INSTALL, cli.STOP, src, arg)
+func (c Code) Stop(m *Message, arg ...string) {
+	m.Cmdy(code.INSTALL, cli.STOP, arg)
+}
+func (c Code) Open(m *Message, arg ...string) {
+	m.ProcessOpen(m.Option(mdb.LINK))
 }
 func (c Code) List(m *Message, src string, arg ...string) {
-	m.Cmdy(code.INSTALL, src, arg)
+	m.Cmdy(code.INSTALL, c.Link(m, src), arg)
 }
-func (c Code) Trash(m *Message, src string, arg ...string) {
-	m.Cmdy(code.INSTALL, nfs.TRASH)
+func (c Code) Trash(m *Message, arg ...string) {
+	m.Cmdy(nfs.TRASH, m.Option(nfs.PATH))
+}
+func (c Code) PushLink(m *Message) *Message {
+	hostname := kit.ParseURLMap(m.Option(ice.MSG_USERWEB))[tcp.HOSTNAME]
+	m.Tables(func(value map[string]string) { m.Push(mdb.LINK, kit.Format("http://%s:%s", hostname, value[tcp.PORT])) })
+	return m
 }
 
 func (c Code) Daemon(m *Message, dir string, arg ...string) {
