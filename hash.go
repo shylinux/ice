34a78335a017914c662f7a2c2512c2d167bae907
harveyshao <harveyshao@tencent.com>
2022-08-01 18:10:32 +0800

    opt base

--- hash.go
+++ hash.go
@@ -2,7 +2,6 @@ package ice
 
 import (
 	"shylinux.com/x/icebergs/base/mdb"
-	kit "shylinux.com/x/toolkits"
 )
 
 type Hash struct {
@@ -11,43 +10,17 @@ type Hash struct {
 	short string `data:""`
 	field string `data:"time,hash,type,name,text"`
 
-	create string `name:"create type name text" help:"创建"`
-	remove string `name:"remove" help:"删除"`
+	create string `name:"create type=hi name=hello text=worl" help:"创建"`
 	list   string `name:"list hash auto create" help:"缓存"`
 }
 
-func (h Hash) Short(m *Message) string {
-	return kit.Select(mdb.HASH, h.Data.Short(m))
-}
-
-func (h Hash) Inputs(m *Message, arg ...string) {
-	h.Data.Inputs(m, mdb.HASH, arg)
-}
-func (h Hash) Create(m *Message, arg ...string) *Message {
-	h.Data.Insert(m, mdb.HASH, arg)
-	return m
-}
-func (h Hash) Remove(m *Message, arg ...string) {
-	h.Data.Delete(m, mdb.HASH, m.OptionSimple(h.Short(m)))
-}
-func (h Hash) Modify(m *Message, arg ...string) {
-	h.Data.Modify(m, mdb.HASH, m.OptionSimple(h.Short(m)), arg)
-}
-func (h Hash) Prunes(m *Message, arg ...string) {
-	h.Data.Prunes(m, mdb.HASH, arg)
-}
-func (h Hash) Export(m *Message, arg ...string) {
-	h.Data.Export(m, mdb.HASH)
-}
-func (h Hash) Import(m *Message, arg ...string) {
-	h.Data.Import(m, mdb.HASH)
-}
+func (h Hash) Inputs(m *Message, arg ...string) { mdb.HashInputs(m.Message, arg) }
+func (h Hash) Create(m *Message, arg ...string) { mdb.HashCreate(m.Message, arg) }
+func (h Hash) Remove(m *Message, arg ...string) { mdb.HashRemove(m.Message, arg) }
+func (h Hash) Modify(m *Message, arg ...string) { mdb.HashModify(m.Message, arg) }
+func (h Hash) Export(m *Message, arg ...string) { mdb.HashExport(m.Message, arg) }
+func (h Hash) Import(m *Message, arg ...string) { mdb.HashImport(m.Message, arg) }
 func (h Hash) List(m *Message, arg ...string) *Message {
-	m.Fields(len(arg), h.Field(m))
-	h.Data.Select(m, mdb.HASH, h.Short(m), arg)
-	m.PushAction(h.Remove)
+	mdb.HashSelect(m.Message, arg...)
 	return m
-}
-func (h Hash) Prev(m *Message, arg ...string) {
-	mdb.PrevPageLimit(m.Message, kit.Select("0", arg, 0), kit.Slice(arg, 1)...)
 }
--- zone.go
+++ zone.go
@@ -1,7 +1,6 @@
 package ice
 
 import (
-	ice "shylinux.com/x/icebergs"
 	"shylinux.com/x/icebergs/base/mdb"
 	kit "shylinux.com/x/toolkits"
 )
@@ -17,48 +16,19 @@ type Zone struct {
 	list   string `name:"list zone id auto insert" help:"存储"`
 }
 
-func (z Zone) Inputs(m *Message, arg ...string) {
-	if kit.Select("", arg, 0) == z.Short(m) {
-		z.Data.Inputs(m, mdb.HASH, arg)
-	} else {
-		z.Data.Inputs(m, mdb.ZONE, m.Option(z.Short(m)), arg)
-	}
-}
-func (z Zone) Create(m *Message, arg ...string) {
-	z.Data.Insert(m, mdb.HASH, arg)
-}
-func (z Zone) Remove(m *Message, arg ...string) {
-	z.Data.Delete(m, mdb.HASH, m.OptionSimple(z.Short(m)))
-}
-func (z Zone) Insert(m *Message, arg ...string) {
-	if m.Conf(m.PrefixKey(), kit.KeyHash(m.Option(z.Short(m)))) == "" {
-		z.Data.Insert(m, mdb.HASH, m.OptionSimple(z.Short(m)))
-	}
-	z.Data.Insert(m, mdb.ZONE, m.Option(z.Short(m)), arg[2:])
-}
-func (z Zone) Modify(m *Message, arg ...string) {
-	z.Data.Modify(m, mdb.ZONE, m.Option(z.Short(m)), m.Option(mdb.ID), arg)
-}
-func (z Zone) Export(m *Message, arg ...string) {
-	if m.OptionFields() == "" {
-		m.OptionFields(m.Config(mdb.SHORT), m.Config(mdb.FIELD))
-	}
-	m.Option(ice.CACHE_LIMIT, "-1")
-	z.Data.Export(m, mdb.ZONE)
-}
-func (z Zone) Import(m *Message, arg ...string) {
-	z.Data.Import(m, mdb.ZONE)
-}
+func (z Zone) Inputs(m *Message, arg ...string) { mdb.ZoneInputs(m.Message, arg) }
+func (z Zone) Create(m *Message, arg ...string) { mdb.ZoneCreate(m.Message, arg) }
+func (z Zone) Remove(m *Message, arg ...string) { mdb.ZoneRemove(m.Message, arg) }
+func (z Zone) Insert(m *Message, arg ...string) { mdb.ZoneInsert(m.Message, arg) }
+func (z Zone) Modify(m *Message, arg ...string) { mdb.ZoneModify(m.Message, arg) }
+func (z Zone) Export(m *Message, arg ...string) { mdb.ZoneExport(m.Message, arg) }
+func (z Zone) Import(m *Message, arg ...string) { mdb.ZoneImport(m.Message, arg) }
 func (z Zone) List(m *Message, arg ...string) *Message {
-	m.Fields(len(arg), kit.Join([]string{mdb.TIME, z.Short(m), mdb.COUNT}), z.Field(m))
-	if z.Data.Select(m, mdb.ZONE, arg); len(arg) == 0 {
-		m.PushAction(z.Remove)
-		m.StatusTimeCount()
-	} else {
-		m.Richs(m.PrefixKey(), "", arg[0], func(key string, value Map) {
-			m.StatusTimeCountTotal(kit.Value(value, kit.Keym(mdb.COUNT)))
-		})
-	}
+	mdb.ZoneSelect(m.Message, arg...)
+	return m
+}
+func (z Zone) ListPage(m *Message, arg ...string) *Message {
+	mdb.ZoneSelectPage(m.Message, arg...).Action(kit.Select("", mdb.PAGE, len(kit.Slice(arg, 0, 2)) > 0))
 	return m
 }
 func (z Zone) Next(m *Message, arg ...string) {
